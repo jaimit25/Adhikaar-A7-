b0bfadaa1e44366f38b0768c66f1b9fb17357892
Janavi Panchal <janavip81@gmail.com>
2021-02-05 23:00:28 +0530

    screens

---
+++ lib/screens/register.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class register extends StatefulWidget {
+  @override
+  _registerState createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
