05999c954e17e03a1bc49cfc6ffae89d72b7114f
Jaimit <68295105+jaimit25@users.noreply.github.com>
2021-06-18 21:25:59 +0530

    Update Showfeeds.dart

--- lib/screens/Showfeeds.dart
+++ lib/screens/Showfeeds.dart
@@ -175,4 +175,30 @@ class _ShowFeedsState extends State<ShowFeeds> {
         .snapshots();
     print('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
   }
+  
+  
+    static getdata() async {
+    print('datadtadatdatdatdatdatadtadtadtadtadtadtadtad');
+    List<FeedModel> list = [];
+    await FirebaseFirestore.instance
+        .collection("Feed")
+        .get()
+        .then((querySnapshot) {
+      querySnapshot.docs.forEach((document) {
+        list.add(FeedModel(
+          email: document['email'],
+          // Head: document['Head'],
+          body: document['body'],
+          likes: document['likes'],
+          Photo: document['Photo'],
+        ));
+      });
+    });
+    print('xxx');
+    print(list.length);
+    print('yyy');
+    return list;
+  }
+  
+  
 }
